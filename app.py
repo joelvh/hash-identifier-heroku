87c0e4b838c9f3da6842796ac166a3a662ffda17
Joel Van Horn <joel@beta.io>
2012-01-26 20:02:53 -0500

    made modifications to Hash ID to make it more modular, call code from web app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from cStringIO import StringIO
 import sys
+import Hash_ID
 
 app = Flask(__name__)
 
@@ -10,15 +11,18 @@ def hello():
     return "Append a hash to the URL (e.g. http://hash-identifier.herokuapp.com/<HASH>)"
 
 @app.route("/<hash>")
-def detect():
+def detect(hash):
     old_stdout = sys.stdout
     sys.stdout = mystdout = StringIO()
 
-    # run hash algorithm
+    Hash_ID.identify(hash)
+    
+    results = mystdout.getvalue()
+    mystdout.close()
 
     sys.stdout = old_stdout
 
-    return mystdout
+    return results
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
